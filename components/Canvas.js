47b9c197a0978f05fe54f7c1ba5bd961810ae4d4
Michaela Lehr <michaela.lehr@geildanke.com>
2017-03-19 12:44:23 +0100

    chore (Canvas): create Canvas component, remove Text component

---
+++ components/Canvas.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  asset,
+  Pano,
+  View,
+} from 'react-vr';
+
+class Canvas extends React.Component {
+
+  render() {
+
+    return (
+      <View>
+        <Pano source={asset('reactconf_0.jpg')}/>
+      </View>
+    );
+  }
+};
+
+module.exports = Canvas;
