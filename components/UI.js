e91a2c0f2fe4ef53bc86617aacb17d72329cff7c
Michaela Lehr <michaela.lehr@geildanke.com>
2017-03-27 20:57:04 +0200

    chore (ui): remove ui animation (out of article scope)

--- components/UI.js
+++ components/UI.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {
-  Animated,
   View,
 } from 'react-vr';
 import Button from './Button';
@@ -13,64 +12,12 @@ class UI extends React.Component {
     super(props);
 
     this.buttons = this.props.buttonConfig;
-    this.state = {
-      animatedOpacity: new Animated.Value(1),
-      animatedRotationX: new Animated.Value(-10),
-    };
-  }
-
-  componentWillMount() {
-    this.fadeOut();
   }
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.buttonConfig) {
       this.buttons = nextProps.buttonConfig;
     }
-  }
-
-  fadeIn() {
-    Animated.parallel([
-      Animated.timing(
-        this.state.animatedOpacity,
-        {
-          toValue: 1,
-          duration: 250,
-          delay: 5000,
-        }
-      ),
-      Animated.timing(
-        this.state.animatedRotationX,
-        {
-          toValue: -10,
-          duration: 250,
-          delay: 5000,
-          easing: Easing.quad,
-        }
-      ),
-    ]).start();
-  }
-
-  fadeOut() {
-    Animated.parallel([
-      Animated.timing(
-        this.state.animatedOpacity,
-        {
-          toValue: 0.25,
-          duration: 250,
-          delay: 5000,
-        }
-      ),
-      Animated.timing(
-        this.state.animatedRotationX,
-        {
-          toValue: -12,
-          duration: 250,
-          delay: 5000,
-          easing: Easing.quad,
-        }
-      ),
-    ]).start();
   }
 
   render () {
@@ -86,20 +33,19 @@ class UI extends React.Component {
       );
 
     return (
-      <Animated.View
+      <View
         style={{
           flexDirection: 'row',
           flexWrap: 'wrap',
-          opacity: this.state.animatedOpacity,
           transform: [
-            {rotateX: this.state.animatedRotationX},
+            {rotateX: -12},
             {translate: [-1.5, 0, -3]},
           ],
           width: 3,
         }}
       >
         {buttons}
-      </Animated.View>
+      </View>
     );
   }
 };
